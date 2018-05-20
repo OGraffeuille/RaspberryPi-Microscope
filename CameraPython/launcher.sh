d2ac09d8dcf8e0f13c9119816b2567c2d2f3fd4a
OGraffeuille <oag1998-trademe2@yahoo.fr>
2018-05-20 16:41:22 +1200

    Removing unnecessary files, namely duplicates, test files and automatic backup files.'

--- CameraPython/launcher.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# launcher.sh
-# Launches python camera module server.
-# Called by run-client-gui.sh and run-client-cmd.sh
-
-cd /home/pi/Documents/PhysicsSummer/CameraPython
-python cameraServerTest.py
-
